522ef324292c7a9d926dcaf885e0104c6aeac14d
David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
2024-05-05 16:54:12 +0200

    Lint EstrellasFugaces.py

--- code/vigenerecracker/EstrellasFugaces.py
+++ code/vigenerecracker/EstrellasFugaces.py
@@ -2,27 +2,27 @@
 #
 # Author: Ralph Gootee <rgootee@gmail.com>
 # Modified by:
-#	- David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
-#	- María Rey Escobar <maria.rescobar@udc.es>
+# 	- David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
+# 	- María Rey Escobar <maria.rescobar@udc.es>
 
-#-------------------------------------------------------------------------------
-from sys import argv
-from math import gcd
+from sys import argv  # for command line arguments
+from math import gcd  # euclidian algorithm
 from functools import reduce
 from collections import deque
 from numpy import linalg, asarray
 
 
-# -------------------------------------------------------------------------------
 def calc_key_length(string, gram_length):
-    """Calculates the suggested passkey length of the string using Kasiski"""
+    """
+    Calculates the suggested passkey length of the string using Kasiski
+    """
 
     locations = []
     distances = []
 
     # locate all grams
     for i in range(len(string)):
-        gram = string[i:i + gram_length]
+        gram = string[i : i + gram_length]
 
         # make sure we don't get stuck with shorties
         if len(gram) < gram_length:
@@ -42,29 +42,31 @@ def calc_key_length(string, gram_length):
 
     return reduce(gcd, distances)
 
+
 def construct_y(string, key_length):
     """
     Constructs the Y_i vectors of string
 
     Input Arguments:
-    string -- the ciphertext
-    keyLength -- the passkey length
+        string -- the ciphertext
+        keyLength -- the passkey length
 
     """
 
     # figure this out
-    y = [''] * key_length
+    y = [""] * key_length
 
-    # Mete cada letra del texto en la posicion de la letra de la clave que la cifro
     for i in range(len(string)):
         y[i % key_length] += string[i]
 
     return y
 
-def calc_mg(y, alfab, prob):
-    """calculates the M_g value for a given string vector"""
 
-    # frequency & probability
+def calc_mg(y, alfab, prob):
+    """
+    Calculates the M_g value for a given string vector
+    """
+
     fre = deque([0] * len(alfab))
     for i in range(len(alfab)):
         fre[i] = y.count(alfab[i])
@@ -73,14 +75,14 @@ def calc_mg(y, alfab, prob):
     for g in range(len(alfab)):
         suma = 0
         for i in range(len(alfab)):
-            suma += prob[i] * fre[i]  # Probabilidad de letra por numero de ocurrencias
-        mg[g] = suma / len(y)  # Se divide el sumatorio entre la longitud de y
-        fre.rotate(-1)  # Desplazamiento a la izquierda
+            suma += prob[i] * fre[i]
+        mg[g] = suma / len(y)
+        fre.rotate(-1)
 
     return mg
 
-# Hackeo de clave
-def attack(string, key_length, prob):
+
+def attack(string, key_length, prob, alfab):
     # construct y[i]
     y = construct_y(string, key_length)
 
@@ -89,6 +91,7 @@ def attack(string, key_length, prob):
     for i in range(key_length):
         m = calc_mg(y[i], alfab, prob)
         mg.append(m)
+
     # suggest the key
     key = ""
     for i in range(key_length):
@@ -96,13 +99,6 @@ def attack(string, key_length, prob):
 
     return key
 
-""" Probabilidades por cada idioma """
-prob_ENG = [.08167, .01492, .02782, .04253, .12702, .02228, .02015, .06094, .06966, .00153, .00772, .04025, .02406,
-            .06749, .0, .07507, .01929, .00095, .05987, .06327, .09056, .02758, .00658, .0236, .0015, .01654, .00074]
-prob_SPN = [.1216, .0149, .0387, .0467, .1408, .0069, .01, 0.018, .0598, .0052, .0011, .0524, .0308, .07, .0, .092,
-            .0289, .0111, .0641, .072, .046, .0469, .0105, .0004, .0014, .0109, .0047]
-prob_FRN = [.08173, .00901, .03345, .03669, .16716, .01066, .00866, .00737, .07579, .00613, .0074, .05456, .02968,
-            .07095, .0, .05837, .02521, .01362, .0693, .07948, .07244, .06429, .01838, .00049, .00427, .00128, .00326]
 
 def detect_language(string):
     """
@@ -126,30 +122,125 @@ def detect_language(string):
         return "French"
     else:
         return "Unknown"
-    
-# Get the input message
-message = input("Enter the encrypted message: ")
 
-# Detect the language of the message
-detected_language = detect_language(message)
 
-# Choose the probabilities and alphabet based on the detected language
-if detected_language == "English":
-    prob = prob_ENG
-    alfab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-elif detected_language == "Spanish":
-    prob = prob_SPN
-    alfab = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-elif detected_language == "French":
-    prob = prob_FRN
-    alfab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def main():
+    """Probabilidades por cada idioma"""
+    prob_ENG = [
+        0.08167,
+        0.01492,
+        0.02782,
+        0.04253,
+        0.12702,
+        0.02228,
+        0.02015,
+        0.06094,
+        0.06966,
+        0.00153,
+        0.00772,
+        0.04025,
+        0.02406,
+        0.06749,
+        0.0,
+        0.07507,
+        0.01929,
+        0.00095,
+        0.05987,
+        0.06327,
+        0.09056,
+        0.02758,
+        0.00658,
+        0.0236,
+        0.0015,
+        0.01654,
+        0.00074,
+    ]
+    prob_SPN = [
+        0.1216,
+        0.0149,
+        0.0387,
+        0.0467,
+        0.1408,
+        0.0069,
+        0.01,
+        0.018,
+        0.0598,
+        0.0052,
+        0.0011,
+        0.0524,
+        0.0308,
+        0.07,
+        0.0,
+        0.092,
+        0.0289,
+        0.0111,
+        0.0641,
+        0.072,
+        0.046,
+        0.0469,
+        0.0105,
+        0.0004,
+        0.0014,
+        0.0109,
+        0.0047,
+    ]
+    prob_FRN = [
+        0.08173,
+        0.00901,
+        0.03345,
+        0.03669,
+        0.16716,
+        0.01066,
+        0.00866,
+        0.00737,
+        0.07579,
+        0.00613,
+        0.0074,
+        0.05456,
+        0.02968,
+        0.07095,
+        0.0,
+        0.05837,
+        0.02521,
+        0.01362,
+        0.0693,
+        0.07948,
+        0.07244,
+        0.06429,
+        0.01838,
+        0.00049,
+        0.00427,
+        0.00128,
+        0.00326,
+    ]
 
-# Calculate the key length
-gram_length = 6
-key_length = calc_key_length(message, gram_length)
+    with open(argv[1], "r", encoding="utf8") as f:
+        message = f.read()
 
-# Perform the attack and get the key
-key = attack(message, key_length, prob)
+    # Detect the language of the message
+    detected_language = detect_language(message)
 
-# Print the key
-print("Decrypted Key:", key)
+    # Choose the probabilities and alphabet based on the detected language
+    if detected_language == "English":
+        prob = prob_ENG
+        alfab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    elif detected_language == "Spanish":
+        prob = prob_SPN
+        alfab = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    elif detected_language == "French":
+        prob = prob_FRN
+        alfab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    # Calculate the key length
+    gram_length = 6
+    key_length = calc_key_length(message, gram_length)
+
+    # Perform the attack and get the key
+    key = attack(message, key_length, prob, alfab)
+
+    # Print the key
+    print("Suggested Passkey:", key)
+
+
+if __name__ == "__main__":
+    main()
